16fb00fab76bce3462ea2f0a02ecc43b21cbe640
Aknagar <akashnagar47@outlook.com>
2018-02-23 23:01:18 +0530

    Removed not used folder

--- grafana/plugins/application-insights-query-tailored/dist/module.d.ts
+++
@@ -1,6 +0,0 @@
-import { AppInsightsDatasource } from './datasource';
-import { AppInsightsQueryCtrl } from './query_ctrl';
-declare class AppInsightsConfigCtrl {
-    static templateUrl: string;
-}
-export { AppInsightsDatasource as Datasource, AppInsightsQueryCtrl as QueryCtrl, AppInsightsConfigCtrl as ConfigCtrl };
